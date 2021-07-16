9c83f1104c3c1f164de6d00151ace01df892eb96
S-c-r-a-t-c-h-y <amormc74@gmail.com>
2021-07-16 22:28:09 +0200

    changes

--- Python/other-test.py
+++
@@ -1,5 +0,0 @@
-
-path = ['test1', 'test2']
-directory = "G:\\Projets divers code\\Python"
-
-print(f'{directory}\\' + '\\'.join(path))
--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,3 @@
 
-
-K, M = 3, 1000
-
-f = lambda x: x ** 2
-s = lambda x: sum(map(f, x)) % 1000
-
-lists = [[5, 4],
-		 [7, 8, 9],
-		 [5, 7, 8, 9, 10]]
-
-for l in lists:
-	l.sort()
-picks = [None] * len(lists)
-
-max = 0
+print( 9%3)
+print( [2] * 5 )
