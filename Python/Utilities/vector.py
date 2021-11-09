c0b99ad9e3c1fd2c9d41ddf8c8989a55b6233439
Scratchy <amormc74@gmail.com>
2021-11-09 23:09:58 +0100

    continued working on my vector implementation

--- Python/Utilities/vector.py
+++ Python/Utilities/vector.py
@@ -1,6 +1,6 @@
+from __future__ import annotations
 import math
 from typing import Union
-from __future__ import annotations
 
 
 class Vector:
@@ -21,7 +21,7 @@ class Vector:
         return self.__mul__(1 / self.length)
 
     def dot(self, other: Vector) -> Union[int, float]:
-        """Returns the dot product of this vector with the other vector."""
+        """Returns the dot product of this vector with the 'other' vector."""
 
         if not isinstance(other, type(self)):
             raise TypeError(f"unsupported operand type(s) for dot product: 'Vector' and '{type(other).__name__}'")
@@ -29,7 +29,7 @@ class Vector:
         return self.x * other.x + self.y * other.y + self.z * other.z
 
     def cross(self, other: Vector) -> Vector:
-        """Returns the cross product of this vector with the other vector."""
+        """Returns the cross product of this vector with the 'other' vector."""
 
         if not isinstance(other, type(self)):
             raise TypeError(f"unsupported operand type(s) for cross product: 'Vector' and '{type(other).__name__}'")
@@ -40,26 +40,79 @@ class Vector:
             self.x * other.y - other.y * self.x,
         )
 
+    def angle_between(self, other: Vector) -> float:
+        """Returns the angle in radians between this vector and the 'other' vector."""
+        if not isinstance(other, type(self)):
+            raise TypeError(
+                f"cannot calculate the angle between an object from type 'Vector' and '{type(other).__name__}'"
+            )
+
+        return math.acos(self.dot(other) / self.length / other.length)
+
+    def collinear(self, other: Vector) -> bool:
+        """Returns wheter or not this vector is collinear with the 'other' vector."""
+        if not isinstance(other, type(self)):
+            raise TypeError(f"'Vector' object cannot be collinear with object from type '{type(other).__name__}'")
+        return self.cross(other) == Vector(0, 0, 0)
+
+    def orthogonal(self, other: Vector) -> bool:
+        """Returns wheter or not this vector is orthogonal with the 'other' vector."""
+        if not isinstance(other, type(self)):
+            raise TypeError(f"'Vector' object cannot be orthogonal with object from type '{type(other).__name__}'")
+        return self.dot(other) == 0
+
+    def scalar_triple_product(self, v1: Vector, v2: Vector) -> Union[int, float]:
+        """Returns the scalar triple product of this vector with the vectors 'v1' and 'v2'."""
+        if not isinstance(v1, type(self)):
+            raise TypeError(
+                f"unsupported operand type(s) for scalar triple product: 'Vector' and '{type(v1).__name__}'"
+            )
+        if not isinstance(v2, type(self)):
+            raise TypeError(
+                f"unsupported operand type(s) for scalar triple product: 'Vector' and '{type(v2).__name__}'"
+            )
+
+        return (
+            self.x * v1.y * v2.z
+            + self.y * v1.z * v2.x
+            + self.z * v1.x * v2.y
+            - self.x * v1.z * v2.y
+            - self.y * v1.x * v2.z
+            - self.z * v1.y * v2.x
+        )
+
+    def coplanar(self, v1: Vector, v2: Vector) -> bool:
+        """Returns wheter or not this vector is coplanar with the vectors 'v1' and 'v2'."""
+        if not isinstance(v1, type(self)):
+            raise TypeError(f"'Vector' object cannot be coplanar with object from type '{type(v1).__name__}'")
+        if not isinstance(v2, type(self)):
+            raise TypeError(f"'Vector' object cannot be coplanar with object from type '{type(v2).__name__}'")
+
+        return self.scalar_triple_product(v1, v2) == 0
+
     def __add__(self, other: Vector) -> Vector:
+        """Returns the result of the addition of this vector with the 'other' vector."""
         if not isinstance(other, type(self)):
             raise TypeError(f"unsupported operand type(s) for +: 'Vector' and '{type(other).__name__}'")
 
         return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other: Vector) -> Vector:
+        """Returns the result of the substraction of this vector with the 'other' vector."""
         if not isinstance(other, type(self)):
             raise TypeError(f"unsupported operand type(s) for -: 'Vector' and '{type(other).__name__}'")
 
         return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __mul__(self, other: Union[int, float]) -> Vector:
+        """Returns the result of the scalar multiplication of this vector with a number 'other'."""
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other, self.z * other)
 
         raise TypeError(f"unsupported operand type(s) for *: 'Vector' and '{type(other).__name__}'")
 
     def __truediv__(self, other: Union[int, float]) -> Vector:
-
+        """Returns the result of the division (scalar multiplication by the inverse) of this vector with a number 'other'."""
         if other == 0:
             raise ZeroDivisionError("Can't divide the vector by 0.")
 
@@ -82,14 +135,16 @@ class Vector:
         return self.x != other.x or self.y != other.y or self.z != other.z
 
     def __bool__(self) -> bool:
+        """True if the vector is not equal to the null vector."""
         return self.x != 0 and self.y != 0 and self.z != 0
 
     def __repr__(self) -> str:
         return f"Vector({self.x}, {self.y}, {self.z})"
 
 
-v1 = Vector(3, -3, 1)
-v2 = Vector(-12, 12, -4)
+if __name__ == "__main__":
+    v1 = Vector(1, 1, 1)
+    v2 = Vector(1, 3, 1)
+    v3 = Vector(2, 2, 2)
 
-v4 = v1.cross(v2)
-print(v4)
+    print(v1.coplanar(v2, v3))
