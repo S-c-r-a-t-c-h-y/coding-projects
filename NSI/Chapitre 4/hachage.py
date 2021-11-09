ff37aefbff803d6de2b73ff44b035f0860eb63c8
Scratchy <amormc74@gmail.com>
2021-11-09 22:26:49 +0100

    Worked on hashing tables

--- NSI/Chapitre 4/hachage.py
+++ NSI/Chapitre 4/hachage.py
@@ -1,23 +1,96 @@
+from liste_chainee import Liste_chainee
+import math
+import numpy as np
+import time
 
-def hacher(mot):
-    code = 0
-    for i, lettre in enumerate(mot):
-        code += (ord(lettre) * (256 ** i))
-    return code % (2**64)
 
-with open("hachage.txt", "r", encoding='utf-8') as f:
+class HashTable:
+    def __init__(self, modulo=2 ** 16):
+        self.__values = b = np.full((modulo,), Liste_chainee())
+        self.__modulo = modulo
+
+    def hash(self, elem):
+        code = sum(math.pi * ord(lettre) * (256 ** i) for i, lettre in enumerate(elem))
+        code = math.floor(code)
+        return code % self.__modulo
+
+    def __contains__(self, key):
+        hash = self.hash(key)
+        head = self.__values[hash].get_tete()
+        while head is not None:
+            if head.get_valeur()[0] == key:
+                return True
+            head = head.get_suivant()
+        return False
+
+    def __getitem__(self, key):
+        if key not in self:
+            return KeyError("Key not in hash table")
+        hash = self.hash(key)
+        head = self.__values[hash].get_tete()
+        while head.get_valeur()[0] != key:
+            head = head.get_suivant()
+        return head.get_valeur()[1]
+
+    def __setitem__(self, key, value):
+        hash = self.hash(key)
+        self.__values[hash].placer((key, value))
+
+    def __delitem__(self, key):
+        if key not in self:
+            return KeyError("Key not found in hash table")
+        hash = self.hash(key)
+        head = self.__values[hash].get_tete()
+        while head.get_valeur()[0] != key:
+            head = head.get_suivant()
+        head = None
+
+
+table = HashTable()
+
+# table["moi"] = 16
+# table["leny"] = 16
+# table["alois"] = 17
+
+# table["FEDS"] = 20
+# table["SOCK"] = 30
+
+# print("moi" in table)
+# print("toi" in table)
+
+# print("FEDS" in table)
+# print("SOCK" in table)
+
+# print(table["leny"])
+# print(table["FEDS"])
+# print(table["SOCK"])
+
+
+# def hacher(elem):
+#     code = sum(math.pi * ord(lettre) * (256 ** i) for i, lettre in enumerate(elem))
+#     code = math.floor(code)
+#     return code % (2 ** 16)
+
+
+with open("hachage.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-dico = {}
 
 for word in text.split():
-    hachage = hacher(word)
-    if hachage in dico:
-        dico[hachage].add(word)
-    else:
-        dico[hachage] = {word}
+    table[word] = 0
 
-# print(dico)
-coeff = sorted([len(valeur) for valeur in dico.values()])
-print(coeff, "\n")
-print({diff: coeff.count(diff) for diff in set(coeff)})
+d = {str(i): 0 for i in range(10000)}
+start = time.time()
+
+for _ in range(1000):
+    # a = "FEDS" in table
+    a = "957" in d
+
+t = time.time() - start
+print(f"{t:.10f}")
+
+
+# coeff = sorted([len(valeur) for valeur in dico.values()])
+# print(coeff, "\n")
+# print({diff: coeff.count(diff) for diff in set(coeff)})
+# print(max_hash)
